75076e999bca15abcbba5bee279a41defb548e78
gkufatty <133120205+gkufatty@users.noreply.github.com>
2024-03-21 00:53:48 -0400

    Update generic-neural-network.py

--- generic-neural-network.py
+++ generic-neural-network.py
@@ -1,30 +1,33 @@
 import os
-import torch
 import glob
 import h5py
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
 import torch.nn as nn
-from collections import OrderedDict
 import matplotlib.pyplot as plt
-from torch.utils.data import random_split
+from helper_functions import accuracy_fn 
 
 
 # Import tqdm for progress bar
 from tqdm.auto import tqdm
-
 from timeit import default_timer as timer 
 
-def print_train_time(start: float, end: float, device: torch.device = None):
-    total_time = end - start
-    print(f"Train time on {device}: {total_time:.3f} seconds")
-    return total_time
+# def print_train_time(start: float, end: float, device: torch.device = None):
+#     total_time = end - start
+#     print(f"Train time on {device}: {total_time:.3f} seconds")
+#     return total_time
 
 # Setting up the device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
-# Defining the HDF5Dataset class
+class_names = ['CCnu_e', 'CCnu_mu', 'NC']
+
+#--------------------------------------------------(1) Getting Datasets Ready--------------------------------------------------
+
+
+
 class HDF5Dataset(Dataset):
     def __init__(self, file_path, transform=None):
         self.file_path = file_path
@@ -68,31 +71,32 @@ class HDF5Dataset(Dataset):
         return self.length
 
     def __getitem__(self, event):
+        print(f"Total events: {len(self.events)}, Total labels: {len(self.labels)}")
         with h5py.File(self.file_path, 'r') as file:
             data_index = self.events[event]  
             data = file['charge/calib_final_hits/data'][data_index]
-            #features = data['x', 'y', 'z']
             features = np.stack([data['x'], data['y'], data['z']], axis=1)  # Combining x, y, z into a single array
             label = self.labels[event]
-            #convert to tensor
-            features = torch.tensor(features, dtype=torch.float)
-            label = torch.tensor(label, dtype=torch.long)
+        #convert to tensor
+        features = torch.tensor(features, dtype=torch.float)
+        label = torch.tensor(label, dtype=torch.long)
         return features, label
 
 
 hdf5_dataset = HDF5Dataset('/home/gkufatty/Documents/research/blip-stuff/flow-files/MiniRun4_1E19_RHC.flow.00000.FLOW.h5')  # Correct path to your HDF5 file
-# check the dataset, dim of features is (N,3)
 
-total_size = len(hdf5_dataset)
-train_size = int(0.8 * total_size)
-test_size = total_size - train_size
+# check the dataset, dim of features is (N,3)
+feat1,label1=hdf5_dataset[0]
+train_size = int(0.8 * len(hdf5_dataset))
+test_size = len(hdf5_dataset)- train_size
 # Split the dataset
 train_dataset, test_dataset = random_split(hdf5_dataset, [train_size, test_size])
+print(f"Total size of dataset: {len(hdf5_dataset)}. Training set: {len(train_dataset)}. Testing set: {len(test_dataset)}")
+print(f'Features shape is: {feat1.shape}')
+
 
 # Create data loaders
-# Setup the batch size hyperparameter
-BATCH_SIZE = 32
-
+BATCH_SIZE = 32 # how many samples per batch?
 train_dataloader = DataLoader(train_dataset, # dataset to turn into iterable
     batch_size=BATCH_SIZE, # how many samples per batch? 
     shuffle=True # shuffle data every epoch?
@@ -103,98 +107,188 @@ test_dataloader = DataLoader(test_dataset,
 )
 
 
-class TestModel(nn.Module):
+
+#------------------training and testing steps------------------
+
+
+def train_step(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn, 
+               device:torch.device=device):
+    train_loss, train_acc= 0,0
+    model.to(device)
+    model.train()
+    for batch, (X,y) in enumerate(data_loader):
+        X,y = X.to(device), y.to(device)
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+        train_acc+=accuracy_fn(y_true=y, 
+                               y_pred=y_pred.argmax(dim=1))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss: {train_loss:.5f} | Train acc: {train_acc:.2f}%")    
+    
+
+def test_step(data_loader: torch.utils.data.DataLoader,
+              model: torch.nn.Module,
+              loss_fn: torch.nn.Module,
+              accuracy_fn,
+              device:torch.device=device):
+    test_loss, test_acc = 0,0
+    model.to(device)
+    model.eval()
+    with torch.inference_mode():
+        for X,y in data_loader:
+            X,y= X.to(device),  y.to(device)
+            test_pred = model(X)
+            loss = loss_fn(test_pred, y)
+            test_loss+=loss
+            test_acc+=accuracy_fn(y_true=y, 
+                                  y_pred=test_pred.argmax(dim=1))   
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)    
+        print(f"Test loss: {test_loss:.5f} | Test acc: {test_acc:.2f}%\n")
+
+
+#------------------baseline model------------------
+
+
+class BaselineModel(nn.Module):
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
         super().__init__()
         self.layer_stack = nn.Sequential(
-            nn.Flatten(), 
-            nn.Linear(in_features=input_shape, out_features=hidden_units), 
+            nn.Flatten(), # neural networks like their inputs in vector form
+            nn.Linear(in_features=input_shape, out_features=hidden_units), # in_features = number of features in a data sample (784 pixels)
             nn.Linear(in_features=hidden_units, out_features=output_shape)
         )
     
     def forward(self, x):
         return self.layer_stack(x)
-    
+
 torch.manual_seed(42)
-
-# Need to setup model with input parameters
-model_0 = TestModel(input_shape=3, 
-    hidden_units=10, # how many units in the hiden layer
-    output_shape=3).to(device) # one for every class
-
-print(model_0) 
-
-
-# Setup loss function and optimizer
+model_1 = BaselineModel(input_shape=3, # number of input features
+    hidden_units=10,
+    output_shape=len(class_names)).to(device) # send model to GPU if it's available
+print(model_1)
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
-
-# Set the seed and start the timer
-torch.manual_seed(42)
-train_time_start_on_cpu = timer()
-
-# Set the number of epochs 
+optimizer = torch.optim.SGD(params=model_1.parameters(), 
+                            lr=0.1)
+# Train and test model 
 epochs = 3
-
-# Create training and testing loop
 for epoch in tqdm(range(epochs)):
-    print(f"Epoch: {epoch}\n-------")
-    ### Training
-    train_loss = 0
+    print(f"Epoch: {epoch}\n---------")
+    train_step(data_loader=train_dataloader, 
+        model=model_1, 
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        accuracy_fn=accuracy_fn,
+        device=device
+    )
+    test_step(data_loader=test_dataloader,
+        model=model_1,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_fn,
+        device=device
+    )
 
-    # Add a loop to loop through training batches
-    for features, labels in train_dataloader:
-        model_0.train() 
-        # 1. Forward pass
-        y_pred = model_0(features)
-
-        # 2. Calculate loss (per batch)
-        loss = loss_fn(y_pred, labels)
-        train_loss += loss # accumulatively add up the loss per epoch 
-
-        # 3. Optimizer zero grad
-        optimizer.zero_grad()
-
-        # 4. Loss backward
-        loss.backward()
-
-        # 5. Optimizer step
-        optimizer.step()
-
-        # Print out how many samples have been seen
-        if batch % 400 == 0:
-            print(f"Looked at {batch * len(features)}/{len(train_dataloader.dataset)} samples")
-
-    # Divide total train loss by length of train dataloader (average loss per batch per epoch)
-    train_loss /= len(train_dataloader)
+# class TestModel(nn.Module):
+#     def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+#         super().__init__()
+#         self.layer_stack = nn.Sequential(
+#             nn.Flatten(), 
+#             nn.Linear(in_features=input_shape, out_features=hidden_units), 
+#             nn.Linear(in_features=hidden_units, out_features=output_shape)
+#         )
     
-    ### Testing
-    # Setup variables for accumulatively adding up loss and accuracy 
-    test_loss, test_acc = 0, 0 
-    model_0.eval()
-    with torch.inference_mode():
-        for X, y in test_dataloader:
-            # 1. Forward pass
-            test_pred = model_0(X)
+#     def forward(self, x):
+#         return self.layer_stack(x)
+    
+# torch.manual_seed(42)
+
+# # Need to setup model with input parameters
+# model_0 = TestModel(input_shape=3, 
+#     hidden_units=10, # how many units in the hiden layer
+#     output_shape=3).to(device) # one for every class
+
+# print(model_0) 
+
+
+# # Setup loss function and optimizer
+# loss_fn = nn.CrossEntropyLoss()
+# optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+
+# # Set the seed and start the timer
+# torch.manual_seed(42)
+# train_time_start_on_cpu = timer()
+
+# # Set the number of epochs 
+# epochs = 3
+
+# # Create training and testing loop
+# for epoch in tqdm(range(epochs)):
+#     print(f"Epoch: {epoch}\n-------")
+#     ### Training
+#     train_loss = 0
+
+#     # Add a loop to loop through training batches
+#     for features, labels in train_dataloader:
+#         model_0.train() 
+#         # 1. Forward pass
+#         y_pred = model_0(features)
+
+#         # 2. Calculate loss (per batch)
+#         loss = loss_fn(y_pred, labels)
+#         train_loss += loss # accumulatively add up the loss per epoch 
+
+#         # 3. Optimizer zero grad
+#         optimizer.zero_grad()
+
+#         # 4. Loss backward
+#         loss.backward()
+
+#         # 5. Optimizer step
+#         optimizer.step()
+
+#         # Print out how many samples have been seen
+#         if batch % 400 == 0:
+#             print(f"Looked at {batch * len(features)}/{len(train_dataloader.dataset)} samples")
+
+#     # Divide total train loss by length of train dataloader (average loss per batch per epoch)
+#     train_loss /= len(train_dataloader)
+    
+#     ### Testing
+#     # Setup variables for accumulatively adding up loss and accuracy 
+#     test_loss, test_acc = 0, 0 
+#     model_0.eval()
+#     with torch.inference_mode():
+#         for X, y in test_dataloader:
+#             # 1. Forward pass
+#             test_pred = model_0(X)
            
-            # 2. Calculate loss 
-            test_loss += loss_fn(test_pred, y) # accumulatively add up the loss per epoch
+#             # 2. Calculate loss 
+#             test_loss += loss_fn(test_pred, y) # accumulatively add up the loss per epoch
 
-            # 3. Calculate accuracy (preds need to be same as y_true)
-            test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+#             # 3. Calculate accuracy (preds need to be same as y_true)
+#             test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
         
-        # Calculations on test metrics need to happen inside torch.inference_mode()
-        # Divide total test loss by length of test dataloader (per batch)
-        test_loss /= len(test_dataloader)
+#         # Calculations on test metrics need to happen inside torch.inference_mode()
+#         # Divide total test loss by length of test dataloader (per batch)
+#         test_loss /= len(test_dataloader)
 
-        # Divide total accuracy by length of test dataloader (per batch)
-        test_acc /= len(test_dataloader)
+#         # Divide total accuracy by length of test dataloader (per batch)
+#         test_acc /= len(test_dataloader)
 
-    ## Print out what's happening
-    print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
+#     ## Print out what's happening
+#     print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
 
-# # Calculate training time      
-train_time_end_on_cpu = timer()
-total_train_time_model_0 = print_train_time(start=train_time_start_on_cpu, 
-                                           end=train_time_end_on_cpu,
-                                           device=str(next(model_0.parameters()).device))
+# # # Calculate training time      
+# train_time_end_on_cpu = timer()
+# total_train_time_model_0 = print_train_time(start=train_time_start_on_cpu, 
+#                                            end=train_time_end_on_cpu,
+#                                            device=str(next(model_0.parameters()).device))
